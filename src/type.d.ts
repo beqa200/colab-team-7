0ec4955ac81d8220aaeb938e54c5ee283ccb17cf
Dachi Natenadze <dachinatenadzeb@gmail.com>
2024-08-13 06:05:56 -0700

    add cart product qynt incearese delete

--- src/type.d.ts
+++ src/type.d.ts
@@ -2,8 +2,13 @@ type DataType = {
   Name: string;
   Price: number;
   Storage: string;
+  img: string;
 };
 export interface MycontextType {
+  Show: boolean;
+  setShow: React.Dispatch<React.SetStateAction<boolean>>;
+  Counter: number;
+  setCounter: React.Dispatch<React.SetStateAction<number>>;
   Data: array;
   setData: React.Dispatch<React.SetStateAction<DataType[]>>;
 }
