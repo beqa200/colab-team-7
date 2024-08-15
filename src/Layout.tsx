d14c5ad439dbc1727b49cb9e5e199ff474f2ef5a
marriamdd <davitashvili.m3@gmail.com>
2024-08-16 02:28:22 +0400

    feat:add user functional

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import HeaderComponent from "./components/mainPage/Header";
 
 export default function Layout() {
   return (
     <>
-      <header></header>
+      <header className="w-full absolute z-50">
+        <HeaderComponent />
+      </header>
       <Outlet />
       <footer></footer>
     </>
